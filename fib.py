6fc7e058686a6105db2316794d8635727ec96bee
bmohanrajbit27 <bmohanraj.bit27@gmail.com>
2017-08-04 13:01:42 +0530

    Update fib.py
    Added sample output

--- fib.py
+++ fib.py
@@ -1,5 +1,17 @@
 '''
      Fibonacci Sequence - Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+     sample output:
+     Enter the up to number that you want produce fib series: 34
+     0
+     1
+     1
+     2
+     3
+     5
+     8
+     13
+     21
+     34
 '''
 a = 0
 b = 1
