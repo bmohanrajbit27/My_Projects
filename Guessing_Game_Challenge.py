c178a701d27c73a14e98a1725f926d7cf88a6a14
bmohanrajbit27 <bmohanraj.bit27@gmail.com>
2018-03-20 12:23:01 +0530

    Update Guessing_Game_Challenge.py

--- Guessing_Game_Challenge.py
+++ Guessing_Game_Challenge.py
@@ -1,7 +1,23 @@
 from random import randint
 
+print('''
+
+    program that picks a random integer from 1 to 100, and has players guess the number. 
+
+    The rules are:
+
+    1.Please select a number between 1 and 100.
+    2.If your first guess is within 10 of the computer guess, you will get a reply as 'warm'
+    3.If your first guess is further 10 away from the computer guess, you will get a reply as 'cold'
+    4.On all subsequent turns, if a guess is
+    5.Closer to the number than the previous guess, you will get "WARMER!"
+    6.Farther from the number than the previous guess, you will get "COLDER! 
+    
+    Once you guessed correctly, you will be congragulated with the number of attempts you took to achieve it
+    ''')
+
 comp_value = randint(1,101)
-print(comp_value)
+#print(comp_value)
 iffirst_guess = True
 guess_list = []
 
@@ -9,7 +25,7 @@ while True:
     
     guess = int(input('Please guess a number: '))
     
-     if guess < 1 or guess > 100:
+    if guess < 1 or guess > 100:
         print('Sorry out of bounds, guess from 1 to 100')
         continue
     elif guess == comp_value:
@@ -24,7 +40,7 @@ while True:
         guess_list.append(abs(guess-comp_value))
         iffirst_guess = False
    
-    elif abs(guess-comp_value) < guess_list[-1] and iffirst_guess == False:
+    elif abs(guess-comp_value) <= guess_list[-1] and iffirst_guess == False:
         guess_list.append(abs(guess-comp_value))
         #print(guess_list)
         print('warmer')
